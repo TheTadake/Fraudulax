41f356ebf73469513a402debd68ccde6ea0c0dda
Roman Pineau <roman.pineau1@gmail.com>
2026-06-02 08:36:23 +0200

    ajout Package + class Personne

---
+++ Projet_Dev2/src/system/personne/Personne.java
@@ -0,0 +1,23 @@
+package system.personne;
+
+public abstract class Personne {
+    private  String num;
+    private  String nom;
+    private String prenom;
+
+    public Personne(String num, String nom, String prenom) {
+        this.num = num;
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    public String getNum() {return num;}
+    public  void setNum(String num) {this.num = num;}
+
+    public String getNom() {return nom;}
+    public void setNom(String nom) {this.nom = nom;}
+
+    public String getPrenom() {return prenom;}
+    public void setPrenom(String prenom) {this.prenom = prenom;}
+}
+
